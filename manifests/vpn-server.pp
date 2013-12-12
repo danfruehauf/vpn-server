412b9f112f65c15a1d4d83e4b2f8bd51b5cad519
Dan Fruehauf <malkodan@gmail.com>
2013-12-12 19:48:46 +1100

    added iodine to main manifest

--- manifests/vpn-server.pp
+++ manifests/vpn-server.pp
@@ -1,5 +1,6 @@
-include vpn-server::xl2tpd
+include vpn-server::iodine
+include vpn-server::openvpn
 include vpn-server::pptpd
 include vpn-server::sshd
-include vpn-server::openvpn
+include vpn-server::xl2tpd
 include vpn-server::firewall
